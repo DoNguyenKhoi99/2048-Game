412bf2dd12bbc17c391aa889e02f51ace8f6e311
DoNguyenKhoi <khoi.do@gemi.io>
2021-06-01 18:51:50 +0700

    update code

--- assets/Scripts/GameManagaer.js
+++ assets/Scripts/GameManagaer.js
@@ -3,19 +3,25 @@ const GAME_CONFIG = {
     ROW: 4,
     COL: 4,
     MARGIN: 16 //results from: (this.mainGame.width - this.block.width * GAME_CONFIG.ROW) / 5;
-}
+};
+
+const ARR_BLOCK = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+];
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
         mainGame: cc.Node,
         block: cc.Prefab,
-        cell: cc.Prefab,
         score: cc.Label,
         recored: cc.Label,
         loseLayout: cc.Node, 
-        _arrBlockPos: [],
-        _arrBlock: []
+        _isChange: false
     },
 
     onLoad() {
@@ -25,76 +31,160 @@ cc.Class({
     },
 
     initObj() {
-        this.initCell();
-        this.initBlock(2);
+        this.initBlock();
+        this.addNum();
+        this.addNum();
     },
 
-    initCell() {
+    initBlock() {
         let y = this.mainGame.height / 2 - GAME_CONFIG.MARGIN,
             x = this.mainGame.width / -2 + GAME_CONFIG.MARGIN;
         let index = 0;    
         for(let row = 0; row < GAME_CONFIG.ROW; row++) {
-            this._arrBlockPos[row] = [];
             for(let col = 0; col < GAME_CONFIG.COL; col++) {
-                this._arrBlockPos[row][col] = {x, y, status: false, index: index++}; //ADD POSITION VALUE
-                this.newCell = cc.instantiate(this.cell); //CREATE PREAFAB CELL
-                this.newCell.setParent(this.mainGame);
-                this.newCell.setPosition(cc.v2(x, y));
-                x += this.newCell.width + GAME_CONFIG.MARGIN;
+                this.newBlock = cc.instantiate(this.block); 
+                this.newBlock.setParent(this.mainGame);
+                this.newBlock.setPosition(cc.v2(x, y));
+                x += this.newBlock.width + GAME_CONFIG.MARGIN;
+                if(ARR_BLOCK[row][col] != 0) {
+                    let label = this.newBlock.getChildByName("Value")
+                    label.getComponent(cc.Label).string = ARR_BLOCK[row][col];
+                }
             }
-            y -= this.newCell.height + GAME_CONFIG.MARGIN;
+            y -= this.newBlock.height + GAME_CONFIG.MARGIN;
             x = this.mainGame.width / -2 + GAME_CONFIG.MARGIN;
         };
     },
-    
-    initBlock(loop) {
-        for(let i = 0; i < loop; i++) {
-            let arrPos = this._arrBlockPos[this.getRandomInt(0, 3)][this.getRandomInt(0, 3)];
-            if(!arrPos.status) {
-                let newBlock = cc.instantiate(this.block);
-                newBlock.setParent(this.mainGame);
-                newBlock.setPosition(cc.v2(arrPos.x, arrPos.y));
-                arrPos.status = true;
-                this._arrBlock.push({block: newBlock, index: arrPos.index});
-            } else {
-                this.initBlock(1);
-            }
-        }
-        cc.error(this._arrBlock);
-    },
 
     onKeyDown(event) {
+        this._isChange = false;
         switch(event.keyCode) {
-            case cc.macro.KEY.left:
-            case cc.macro.KEY.right:
-                this.checkRow(event.keyCode);
+            case 37:
+            case 39:
+                this.checkLeftRight(event.keyCode);  
                 break;
-            case cc.macro.KEY.up:
-            case cc.macro.KEY.down:
-                this.checkCol(event.keyCode);               
+            case 38: 
+            case 40:
+                this.checkUpDown(event.keyCode);
                 break;
+        }
+    },
+
+    slideLeftOrUp(array) {
+        let newArray = [];
+        for(let row = 0; row < GAME_CONFIG.ROW; row++) {
+            if(array[row] != 0) newArray.push(array[row]);
         };
+        for(let col = newArray.length; col < GAME_CONFIG.COL; col++){
+            newArray.push(0);
+        }
+        return newArray;
     },
 
-    checkRow(value) {
+    slideRightOrDown(array) {
+        let newArray = [];
         for(let row = 0; row < GAME_CONFIG.ROW; row++) {
-            for(let col = 0; col < GAME_CONFIG.COL; col++) {
-                
+            if(array[row] == 0) newArray.push(array[row]);
+        };
+        for(let row = 0; row < GAME_CONFIG.ROW; row++) {
+            if(array[row] != 0) newArray.push(array[row]);
+        };
+        return newArray;
+    },
+
+    addNum() {
+        let newArr = [];
+        for(let row = 0; row < GAME_CONFIG.ROW; row++) {
+            for(let col = 0; col < GAME_CONFIG.COL; col++) { 
+                if(ARR_BLOCK[row][col] == 0) {
+                    newArr.push({x: row, y: col});
+                }
+            }
+        }
+        if(newArr.length > 0) {
+            let randomXY = newArr[Math.random() * newArr.length >> 0];
+            let number = Math.floor(Math.random() * 4);
+            number < 3 ? ARR_BLOCK[randomXY.x][randomXY.y] = 2 : ARR_BLOCK[randomXY.x][randomXY.y] = 4;   
+        }
+        this.initBlock();
+    },
+
+    hasChangeArray(arr1, arr2) {
+        for(let row = 0; row < GAME_CONFIG.ROW; row++) {
+            if(arr1[row] != arr2[row]) {
+                this._isChange = true;
             }
         }
     },
 
-    checkCol(value) {
+    checkLeftRight(keyCode) {
         for(let row = 0; row < GAME_CONFIG.ROW; row++) {
+            let arr = ARR_BLOCK[row];
+            ARR_BLOCK[row] = this.slideLeftOrUp(ARR_BLOCK[row]);
+            keyCode == 37 ? this.forLeft(row) : this.forRight(row);
+            ARR_BLOCK[row] = this.slideLeftOrUp(ARR_BLOCK[row]);
+            this.hasChangeArray(arr, ARR_BLOCK[row]);
+        }
+        if(this._isChange) {
+            this.addNum();
+        }
+        this.initBlock();
+    },
+
+    checkUpDown(keyCode) {
+        for(let row = 0; row < GAME_CONFIG.ROW; row++) {
+            let newArr = [];
             for(let col = 0; col < GAME_CONFIG.COL; col++) {
-                cc.error(this._arrBlockPos[col][row]);
+                newArr.push(ARR_BLOCK[col][row]);    
+            }
+            let arr  = newArr;
+            newArr = this.slideLeftOrUp(newArr);
+            keyCode == 38 ? this.forUp(newArr) : this.forDown(newArr);
+            newArr = this.slideLeftOrUp(newArr);
+            for(let i = 0; i < 4; i++) {
+                ARR_BLOCK[i][row] = newArr[i];
+            }
+            this.hasChangeArray(arr, newArr);
+        }
+        if(this._isChange) {
+            this.addNum();
+        }
+        this.initBlock();
+    },
+
+    forUp(newArr) {
+        for(let m = 0; m < 3; m++) {
+            if(newArr[m] == newArr[m + 1]) {
+                newArr[m] += newArr[m + 1];
+                newArr[m + 1] = 0;
             }
         }
     },
 
-    getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }, 
+    forDown(newArr) {
+        for(let m = 3; m > 0; m--) {
+            if(newArr[m] == newArr[m - 1]) {
+                newArr[m] += newArr[m - 1];
+                newArr[m - 1] = 0;
+            }
+        }
+    },
+
+    forLeft(row) {
+        for(let col = 0; col < 3; col++) {
+            if(ARR_BLOCK[row][col] == ARR_BLOCK[row][col + 1]) {
+                ARR_BLOCK[row][col] += ARR_BLOCK[row][col + 1];
+                ARR_BLOCK[row][col + 1] = 0;
+            }
+        }
+    },
+
+    forRight(row) {
+        for(let col = 3; col > 0; col--) {
+            if(ARR_BLOCK[row][col] == ARR_BLOCK[row][col - 1]) {
+                ARR_BLOCK[row][col] += ARR_BLOCK[row][col - 1];
+                ARR_BLOCK[row][col - 1] = 0;
+            }
+        }
+    }
 });
